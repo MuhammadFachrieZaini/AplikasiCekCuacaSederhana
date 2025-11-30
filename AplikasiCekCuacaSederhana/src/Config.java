d88d65026c1367b7b8e6f771c7c2cdb94d5b1ee7
MuhammadFachrieZaini <mfachriezn@gmail.com>
2025-11-30 20:29:51 +0700

    Add files via upload

---
+++ AplikasiCekCuacaSederhana/src/Config.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Config {
+    // Ganti nilai ini dengan Kunci API OpenWeatherMap Anda yang sebenarnya
+    public static final String OPEN_WEATHER_MAP_API_KEY = "042ec8689a1d1dfaba84af0c6e14d088";
+}
